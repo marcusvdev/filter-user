6f85204dc16195ba9ed9635d89746f454cf23c34
Marcus Vinicius <marcusvdev@gmail.com>
2020-05-28 21:21:47 -0300

    Init script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,20 @@
-const peoples = fetch('https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo');
-
 window.addEventListener('load', () => {
-
-    console.log(peoples);
-
+    fetchApi();
 });
+
+async function fetchApi(){
+    const api = await fetch('https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo');
+    const json = await api.json();
+    users = json.results.map(user => {
+        const { name, picture, dob, gender, email, location } = user;
+        return {
+            name: name.first + ' ' + name.last,
+            picture: picture.large,
+            age: dob.age,
+            gender: gender,
+            email: email,
+            country: location.country,
+            city: location.city
+        };
+    });
+}
